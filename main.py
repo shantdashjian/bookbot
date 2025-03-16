d63c67027d1adb692b4237c9c500851101314a98
Shant Dashjian <shantgdashjian@gmail.com>
2025-03-16 16:52:18 +0400

    Add report

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sort_dictionary
     
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     dictionary = get_num_characters(text)
-    print(f"{num_words} words found in the document")
-    print(dictionary)
+    sorted_dictionary = sort_dictionary(dictionary)
 
+    # Generate Report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("# --------- Character Count -------")
+    for entry in sorted_dictionary:
+        key = entry["char"]
+        value = entry["num"]
+        print(f"{key}: {value}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -6,8 +6,26 @@ def get_num_characters(text):
     dictionary = {}
     for char in text:
         key = char.lower()
+        if key in [" ",  "\n"]:
+            continue
         if key in dictionary:
             dictionary[key] += 1
         else:
             dictionary[key] = 1
     return dictionary
+
+def sort_on(item):
+    return item["num"]
+
+def convert_to_list_of_dictionaries(dictionary):
+    list_of_dictionaries = []
+    for key in dictionary:
+        list_of_dictionaries.append({
+            "char": key,
+            "num": dictionary[key]
+        })
+    return list_of_dictionaries
+
+def sort_dictionary(dictionary):
+    list_of_dictionaries = convert_to_list_of_dictionaries(dictionary)
+    return sorted(list_of_dictionaries, key=sort_on, reverse=True)
