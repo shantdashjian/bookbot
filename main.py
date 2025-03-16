16b7ba3b1183c5e2b2ba750201c91cb2c5af642f
Shant Dashjian <shantgdashjian@gmail.com>
2025-03-16 17:00:55 +0400

    Ensure book path entry

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from stats import get_num_words, get_num_characters, sort_dictionary
-    
+import sys
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     dictionary = get_num_characters(text)
