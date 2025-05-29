58063e38ccc64fc269a2f62a13d4e5e6c2d2355e
Shant Dashjian <shantgdashjian@gmail.com>
2025-05-29 11:00:16 +0400

    Get character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_dictionary
 
 
 def get_book_text(filepath):
@@ -8,7 +8,11 @@ def get_book_text(filepath):
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
+
     num_words = get_num_words(text)
+    char_dictionary = get_char_dictionary(text)
+
     print(f"{num_words} words found in the document")
+    print(char_dictionary)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_dictionary(text):
+    dict = {}
+    for char in text:
+        char = char.lower()
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
