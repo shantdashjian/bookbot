b9d12641a024adde3381af1fc68b71165757d999
Shant Dashjian <shantgdashjian@gmail.com>
2025-05-29 12:31:43 +0400

    Add path to book as program argument

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import sys
 from stats import get_num_words, get_char_dictionary
 from io_utils import get_book_text, print_report
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
+    text = get_book_text(path_to_book)
     num_words = get_num_words(text)
     char_dictionary = get_char_dictionary(text)
-    print_report(filepath, num_words, char_dictionary)
+    print_report(path_to_book, num_words, char_dictionary)
     
 main()
