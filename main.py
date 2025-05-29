92b2f8f956006096b89565b1e8feb9ab62400826
Shant Dashjian <shantgdashjian@gmail.com>
2025-05-29 10:53:59 +0400

    Move the stats functions to a separate file

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from stats import get_num_words
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
         return text
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
