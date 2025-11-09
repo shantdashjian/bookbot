3e14d57211f12c338153861815df8d4be960e7e9
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-09 20:47:43 +0400

    Read file and return text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        text = f.read()
+    return text
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
