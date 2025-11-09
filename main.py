de0cf283c88bba4cac843ff564db8d42787f45cd
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-09 21:24:59 +0400

    Read book file path from the command line

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_char_count, get_sorted_list_of_char_count
 
 def get_book_text(path):
@@ -6,7 +7,10 @@ def get_book_text(path):
     return text
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_num_words(text)
     char_count = get_char_count(text)
